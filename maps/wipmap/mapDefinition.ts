e7a33a6b1dfdca8041f72a198f0023bff02f1bac
Harmonic <harmonic431@gmail.com>
2026-03-17 15:22:47 +0000

    Add Mystery Box locations to wipmap in zones 1, 3, and 4

--- maps/wipmap/mapDefinition.ts
+++ maps/wipmap/mapDefinition.ts
@@ -168,7 +168,11 @@ export const WipmapDefinition: MapDefinition = {
         ],
         perks: [],
         wallbuys: [],
-        mysteryBoxes: [],
+        mysteryBoxes: [
+            { pos: [-11.5, 0, -8], rotation: Math.PI / 2 },
+            { pos: [36.5, 0, 32], rotation: -Math.PI / 2 },
+            { pos: [36.5, 0, -32], rotation: -Math.PI / 2 }
+        ],
         buildings: []
     },
 
