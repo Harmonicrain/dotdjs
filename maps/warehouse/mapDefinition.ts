13fe3fdfe2f0ddd57a01b993b264a6ee4c5f114c
Harmonic <harmonic431@gmail.com>
2026-03-20 20:12:33 +0000

    Reposition Warehouse machines for cleaner wall placement

--- maps/warehouse/mapDefinition.ts
+++ maps/warehouse/mapDefinition.ts
@@ -65,7 +65,7 @@ export const WarehouseMapDefinition: MapDefinition = {
             { type: "speed_cola", id: "speedCola", zone: 3, pos: [28, 0, 2], rotation: -Math.PI / 2 },
             { type: "quick_revive", id: "quickRevive", zone: 1, pos: [-3, 0, -28.9], rotation: 0 },
             { type: "double_tap", id: "doubleTap", zone: 2, pos: [-9.25, 0, 21.16], rotation: Math.PI / 2 },
-            { type: "mule_kick", id: "muleKick", zone: 3, pos: [28, 0, -8], rotation: -Math.PI / 2 }
+            { type: "mule_kick", id: "muleKick", zone: 3, pos: [18.4, 0, -4.25], rotation: 0 }
         ],
         wallbuys: [
             { weapon: "shotgun", cost: 500, id: "buy_shotgun", zone: 1, pos: [-9.4, 2.5, -15], rotation: Math.PI / 2 },
@@ -81,7 +81,7 @@ export const WarehouseMapDefinition: MapDefinition = {
             rotation: Math.PI / 2,
             powerDoor: { pos: [-10, 3, -5], size: [0.4, 6, 4], openY: 8, connects: [1, 4] }
         },
-        packAPunch: { pos: [-14.0, 0, -2.5], rotation: Math.PI / 2, zone: 4 }
+        packAPunch: { pos: [-15.0, 0, -4.5], rotation: Math.PI / 2, zone: 4 }
     },
 
     fixtures: [
